922f949be5504d3b09994690e530dbc5b768127f
aminarehman14 <46981456+aminarehman14@users.noreply.github.com>
2019-09-20 10:17:51 -0500

    Material Angular Responsive NavBAr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { LayoutComponent } from "./layout/layout.component";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angularNavBar';
+  title = "angularNavBar";
 }
